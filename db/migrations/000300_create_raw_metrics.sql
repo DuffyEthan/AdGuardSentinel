516ff6c8423a94f05b4cb963752029fcbb86a895
Alex L <aleung@tcd.ie>
2026-02-11 17:39:22 +0900

    is already hypertable check

--- db/migrations/000300_create_raw_metrics.sql
+++ db/migrations/000300_create_raw_metrics.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS raw_metrics(
   PRIMARY KEY (publisher_id, bucket_timestamp)
 );
 
-SELECT create_hypertable('raw_metrics', 'bucket_timestamp');
+SELECT create_hypertable('raw_metrics', 'bucket_timestamp',if_not_exists => TRUE);
 
 -- migrate:down
 DROP TABLE IF EXISTS raw_metrics;
--- db/migrations/000400_create_model_logs.sql
+++ db/migrations/000400_create_model_logs.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS model_logs(
   PRIMARY KEY (publisher_id, timestamp)
 );
 
-SELECT create_hypertable('model_logs', 'timestamp');
+SELECT create_hypertable('model_logs', 'timestamp', if_not_exists => TRUE);
 
 -- migrate:down
 DROP TABLE IF EXISTS model_logs;
