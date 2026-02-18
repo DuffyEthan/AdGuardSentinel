7a18f0c884b533bc15f522189afb18844d740270
Alex L <aleung@tcd.ie>
2026-02-18 17:14:49 +0900

    added model_log_repo

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends
 from datetime import datetime, timezone
 from app.db.session import get_session
+
+from app.db import SessionLocal
+from app.db.models import ModelLogs
 from app.db.models import RawMetrics
 from app.repositories.model_logs_repository import ModelLogsRepository
 from app.repositories.raw_metrics_repository import RawMetricsRepository
@@ -10,33 +13,6 @@ from sqlalchemy.orm import Session
 import os
 
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-print("DATABASE_URL:",DATABASE_URL)
-
-if DATABASE_URL:
-    # Use psycopg v3 driver (not psycopg2) with SQLAlchemy
-    SQLALCHEMY_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
-    engine = create_engine(SQLALCHEMY_URL, pool_pre_ping=True)
-else:
-    engine = None
-
-from app.db.models import Base
-Base.metadata.create_all(bind=engine)
-    
-SessionLocal = sessionmaker(bind=engine) if engine else None
-print("SessionLocal type:", type(SessionLocal))
-
-session=SessionLocal()
-print("db_session type:", type(session))  
-
-session: Session = next(get_session())
-t=datetime(2026, 1, 1, 4, 0, tzinfo=timezone.utc)
-n=5
-publisher_id=1
-
-print(session.query(RawMetrics).all())
-# print(RawMetricsRepository(session).get_last_n_before(t, n, publisher_id))
 
 app = FastAPI()
 
@@ -52,14 +28,15 @@ def model_logs_get_between():
 def model_logs_get_between():
     return {"0":"get-last-n-before"}
 
-# @app.get("/model-logs/get-between")
-# def model_logs_get_between(
-#     t1: datetime,
-#     t2: datetime,
-#     publisher_id: int,
-#     session: Session = Depends(get_session)
-# ):
-#     return ModelLogsRepository(session).get_between(t1, t2, publisher_id)
+@app.get("/model-logs/get-between")
+def model_logs_get_between(
+    t1: datetime,
+    t2: datetime,
+    publisher_id: int,
+    session: Session = Depends(get_session)
+):
+    print(ModelLogsRepository(session).get_between(t1, t2, publisher_id))
+    return ModelLogsRepository(session).get_between(t1, t2, publisher_id)
 
 @app.get("/raw-metrics/get-last-n-before")
 def raw_metrics_get_last_n_before(
@@ -68,6 +45,6 @@ def raw_metrics_get_last_n_before(
     publisher_id: int,
     session: Session = Depends(get_session)
 ):
+    print(RawMetricsRepository(session).get_last_n_before(t, n, publisher_id))
     return RawMetricsRepository(session).get_last_n_before(t, n, publisher_id)
-
 
--- app/repositories/model_logs_repository.py
+++ app/repositories/model_logs_repository.py
@@ -1,4 +1,4 @@
-from collections.abc import Sequence
+# from collections.abc import Sequence
 from datetime import datetime
 
 from sqlalchemy import Row, asc
@@ -13,18 +13,21 @@ class ModelLogsRepository(BaseRepository):
         t1: datetime, # inclusive lower bound timestamp
         t2: datetime, # inclusive upper bound timestamp
         publisher_id: int | None = None, # optionally filter by publisher_id
-    ) -> Sequence[Row[tuple[ModelLogs, RawMetrics]]]:
+    # ) -> Sequence[Row[tuple[ModelLogs, RawMetrics]]]:
+    ) -> list[ModelLogs]:
         query = (
-            self.session.query(ModelLogs, RawMetrics)
-            .join(
-                RawMetrics,
-                ModelLogs.publisher_id == RawMetrics.publisher_id,
-            )
+            self.session.query(ModelLogs
+                               # , RawMetrics
+                               )
+            # .join(
+            #     RawMetrics,
+            #     ModelLogs.publisher_id == RawMetrics.publisher_id,
+            # )
             .filter(
                 ModelLogs.timestamp >= t1,
                 ModelLogs.timestamp <= t2,
-                RawMetrics.bucket_timestamp >= t1,
-                RawMetrics.bucket_timestamp <= t2,
+                # RawMetrics.bucket_timestamp >= t1,
+                # RawMetrics.bucket_timestamp <= t2,
             )
         )
 
