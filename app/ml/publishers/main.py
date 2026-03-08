ff979f0ed598dec48497a0a3281ba3285d6f4c29
Alex L <aleung@tcd.ie>
2026-03-09 07:52:58 +0900

    created template for publisher

---
+++ app/ml/publishers/main.py
@@ -0,0 +1,16 @@
+from app.ml.publishers.pub_cat import pub_cat
+from app.ml.publishers.pub_dog import pub_dog
+from app.ml.publishers.pub_fox import pub_fox
+from app.ml.publishers.pub_owl import pub_owl
+from app.ml.publishers.pub_spy import pub_spy
+
+publisherCatalog=[
+    pub_cat,
+    pub_dog,
+    pub_fox,
+    pub_owl,
+    pub_spy
+]
+
+if __name__=="__main__":
+    print("Hello, World!")
---
+++ app/ml/publishers/pub_cat.py
@@ -0,0 +1,26 @@
+from app.ml.markov import MarkovDataGenerator
+
+# taken from app.ml.markov
+
+# dummy functions that just print the current state
+def foo(_):
+    print("foo")
+    return (0,0,0,0)
+
+def bar(_):
+    print("bar")
+    return (0,0,0,0)
+
+# dict representation of our graph with functions for each node
+bimodal = {
+    "normal": {
+        "function": foo,
+        "edges": [("normal", 0.95), ("abnormal", 0.05)]
+    },
+    "abnormal": {
+        "function": bar,
+        "edges": [("abnormal", 0.95), ("normal", 0.05)]
+    }
+}
+
+pub_cat = MarkovDataGenerator.from_dict(bimodal, "normal")
