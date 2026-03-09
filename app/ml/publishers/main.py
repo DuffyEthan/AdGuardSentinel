10d14351cc458d04bf42dcd34acf5635a6640d01
Alex L <aleung@tcd.ie>
2026-03-10 00:28:26 +0900

    move publisherCatalog from publishers.main to publishers

--- app/ml/publishers/main.py
+++ app/ml/publishers/main.py
@@ -1,16 +1,6 @@
-from app.ml.publishers.pub_cat import pub_cat
-from app.ml.publishers.pub_dog import pub_dog
-from app.ml.publishers.pub_fox import pub_fox
-from app.ml.publishers.pub_owl import pub_owl
-from app.ml.publishers.pub_spy import pub_spy
-
-publisherCatalog={
-    "cat":pub_cat,
-    "dog":pub_dog,
-    "fox":pub_fox,
-    "owl":pub_owl,
-    "spy":pub_spy
-}
+from app.ml.publishers import publisherCatalog
 
 if __name__=="__main__":
+    print(publisherCatalog)
     print("Hello, World!")
+    
